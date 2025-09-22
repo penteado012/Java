cc7791c436fce0d81ed35e70ec93474b1d853a5b
Matheus Penteado <matheuspent12@gmail.com>
2025-09-22 20:02:20 -0300

    Delete

--- Trabalhos_4ºSemestre/Exercícios_Polimorfismo/Polimorfismo/src/Figuras.java
+++
@@ -1,9 +0,0 @@
-public interface Figuras {
-    public String nomeInterface = "Figuras";    
-
-    abstract public String getNome();
-    abstract public void setNome(String nome);
-    public double getArea();
-    public double getPerimetro();
-    double getDiagonal();
-}
--- Trabalhos_4ºSemestre/Exercícios_Polimorfismo/Polimorfismo/src/Retangulo2.java
+++
@@ -1,6 +0,0 @@
-public class Retangulo2 extends Quadrilatero {
-    public Retangulo2(double lado1, double lado2){
-        super(lado1, lado2);
-        super.ID = "Retângulo";
-    }
-}
